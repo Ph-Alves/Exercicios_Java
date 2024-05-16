462e8fe906eec2587481669e91f679ca259558bb
Ph-Alves <124298900+Ph-Alves@users.noreply.github.com>
2024-05-16 14:10:48 -0300

    Exercício "Employee" - Herança e Polimorfismo
    Superclasse employee e subclasse outsourced que com suas respectivas variáveis, getters e setters e encapsulamento, além de override de métodos específicos, elas garantem o bom funcionamento do program mainEmployee.

---
+++ heranca_polimorfismo_udemy/src/entities/Employee.java
@@ -0,0 +1,54 @@
+package entities;
+
+public class Employee {
+	
+	protected String name;
+	protected Integer hours;
+	protected Double valuePerHour;
+	
+	public Employee() {
+	}
+
+	public Employee(String name, Integer hours, Double valuePerHour) {
+		this.name = name;
+		this.hours = hours;
+		this.valuePerHour = valuePerHour;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(Double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public Double payment() {
+		return valuePerHour * hours;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(name);
+		sb.append(" - $ ");
+		sb.append(String.format("%.2f", payment()));
+		return sb.toString();
+	}
+	
+}
