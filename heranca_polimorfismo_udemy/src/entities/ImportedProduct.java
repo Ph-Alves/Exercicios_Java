95571707970d96ad347ffbdacb99961d2bda5f45
Ph-Alves <124298900+Ph-Alves@users.noreply.github.com>
2024-05-17 17:44:26 -0300

    Exercício "Product" - Exercício de fixação
    Arquivos de códigos das classes do mainProduct, contendo uma superclasse(Product) e duas subclasses (Imported e OutSourced) cada classe tem seus métodos e variáveis porém com as subclasses recebendo tudo da superclasse.

---
+++ heranca_polimorfismo_udemy/src/entities/ImportedProduct.java
@@ -0,0 +1,32 @@
+package entities;
+
+public final class ImportedProduct extends Product {
+
+	private Double customsFee;
+	
+	public ImportedProduct() {
+		super();
+	}
+
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() + " $ " + totalPrice() + " (Customs fee: $ " + customsFee + ")"; 
+	}
+	
+	public Double totalPrice() {
+		return getPrice() + customsFee;
+	}
+}
